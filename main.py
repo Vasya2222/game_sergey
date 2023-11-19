c8b78ad50260105e76276ead610064333fe0a1d7
Vasya2222 <116379970+Vasya2222@users.noreply.github.com>
2023-11-19 11:02:36 +0300

    dsaf

--- main.py
+++ main.py
@@ -1,140 +1,148 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-
 import pygame
 
 
-# Игрок
-class Player:
-    def __init__(self, screen_width, screen_height):
-        # Размеры
-        # Игрок занимает 1/14 по ширине и 1/10 по высоте игрового экрана
-        # Игровой спрайт
-        self.__sprite = pygame.transform.smoothscale(
-            pygame.image.load("sprites/shish.png").convert_alpha(),
-            (screen_width // 14, screen_height // 10)
-        )
-        self.__rect = self.__sprite.get_rect()
-        # Положение
-        self.__rect.x = screen_width // 2 - self.__rect.width // 2
-        self.__rect.y = screen_height // 2 - self.__rect.height // 2
-        # Движение
-        self.__horizontal_move_flag = self.__vertical_move_flag = 0
-        self.__speed = 10
-
-    def check_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                self.__horizontal_move_flag = -1
-            elif event.key == pygame.K_d:
-                self.__horizontal_move_flag = 1
-            elif event.key == pygame.K_w:
-                self.__vertical_move_flag = -1
-            elif event.key == pygame.K_s:
-                self.__vertical_move_flag = 1
-
-    def check_logic(self, screen_width, screen_height):
-        if self.__rect.x < 0:
-            self.__rect.x = 0
-        elif self.__rect.x > screen_width - self.__rect.width:
-            self.__rect.x = screen_width - self.__rect.width
-        if self.__rect.y < 0:
-            self.__rect.y = 0
-        elif self.__rect.y > screen_height - self.__rect.height:
-            self.__rect.y = screen_height - self.__rect.height
-
-    def check_collision(self, enemy):
-        return self.__rect.colliderect(enemy.rect)
-
-    def move(self):
-        self.__rect.x += self.__speed * self.__horizontal_move_flag
-        self.__rect.y += self.__speed * self.__vertical_move_flag
-        self.__horizontal_move_flag = 0
-        self.__vertical_move_flag = 0
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), self.__rect)
-        screen.blit(self.__sprite, self.__rect)
-
-
-class Enemy:
-    def __init__(self, x, y, screen_width, screen_height):
-        # Размеры
-        # Враг занимает 1/14 по ширине и 1/10 по высоте игрового экрана
-        # Игровой спрайт
-        self.__sprite = pygame.transform.smoothscale(
-            pygame.image.load("sprites/badshish.png").convert_alpha(),
-            (screen_width // 14, screen_height // 10)
-        )
-        self.rect = self.__sprite.get_rect()
-        # Положение
-        self.rect.x = x
-        self.rect.y = y
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 255, 255), self.rect)
-        screen.blit(self.__sprite, self.rect)
-
-
 class Game:
-    def __init__(self, width=800, height=600, fps=60):
+    def __init__(self, w=800, h=400, c=(80, 22, 100), fps=50):
         pygame.init()
+        self.count = 0
+        self.width = w
+        self.heidth = h
+        self.color = c
+        self.fps = fps
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.width, self.heidth))
+        self.game_and = False
+        self.x_rect = self.width // 2
+        self.y_rect = self.heidth // 2
+        self.x_rect2 = 340
+        self.y_rect2 = 200
+        self.width_rect = 20
+        self.heidth_cert = 10
+        self.color2 = (255, 0, 0)
+        self.rect= pygame.Rect
+        self.width_rect2 = 20
+        self.heidth_cert2 = 10
+        self.color3 = (0, 0, 255)
+        self.rect2 = pygame.Rect
+        self.speed = 10
+        self.speed2 = 10
+        self.horizontal_move_flag = self.vertical_move_flag = 0
+        self.horizontal_move_flag2 = self.vertical_move_flag2 = 0
 
-        self.__width = width
-        self.__height = height
-        self.__screen = pygame.display.set_mode((self.__width, self.__height))
-        self.__bg_sprite = pygame.transform.smoothscale(
-            pygame.image.load("sprites/bg.jpg").convert(),
-            (self.__width, self.__height)
-        )
-        self.__fps = fps
-        self.__clock = pygame.time.Clock()
-        self.__game_end = False
+    def func_rect(self):
+        self.rect = pygame.draw.rect(self.screen, self.color2,
+                                      (self.x_rect, self.y_rect, self.width_rect2, self.heidth_cert2))
 
-        self.__player = Player(self.__width, self.__height)
-        self.__enemy = Enemy(150, 150, self.__width, self.__height)
+    def func_rect2(self):
+        self.rect2 = pygame.draw.rect(self.screen, self.color3,
+                                      (self.x_rect2, self.y_rect2, self.width_rect2, self.heidth_cert2))
+
+    def draw(self):
+
+        self.screen.fill(self.color)
+        self.func_rect()
+        self.func_rect2()
+        pygame.display.flip()
+
+    def zikl(self):
+        while not self.game_and:
+
+            self.clock.tick(self.fps)
+            self.check_event()
+            self.move()
+            self.move2()
+            self.logek2(self.width, self.heidth)
+            self.logek(self.width, self.heidth)
+            self.draw()
+            self.collision(self.rect, self.rect2)
+
 
     def __del__(self):
         pygame.quit()
 
-    def run(self):
-        while not self.__game_end:
-            self.__check_events()
-            self.__check_logic()
-            self.__move_objects()
-            self.__draw()
-
-            self.__clock.tick(self.__fps)
-
-    def __check_events(self):
+    def check_event(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.__game_end = True
+            if event == pygame.QUIT:
+                self.game_and = True
+            self.event(event)
+            self.event2(event)
 
-            self.__player.check_event(event)
+    # def rect(self, x_rect, y_rect, color2=(255, 0, 0), width_rect=20, heidth_cert=10):
+    #     self.my_rect = pygame.draw.rect(self.screen, color2, (x_rect, y_rect, width_rect, heidth_cert))
 
-    def __check_logic(self):
-        self.__player.check_logic(self.__width, self.__height)
+    def move(self):
+        self.x_rect += self.speed * self.horizontal_move_flag
+        self.y_rect += self.speed * self.vertical_move_flag
+        self.horizontal_move_flag = 0
+        self.vertical_move_flag = 0
 
-        if self.__player.check_collision(self.__enemy):
-            print("Вы столкнулись с Плохишишем!")
-            self.__game_end = True
+    def event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.horizontal_move_flag = -1
+            elif event.key == pygame.K_RIGHT:
+                self.horizontal_move_flag = 1
+            elif event.key == pygame.K_UP:
+                self.vertical_move_flag = -1
+            elif event.key == pygame.K_DOWN:
+                self.vertical_move_flag = 1
+            elif event.key == pygame.K_SPACE:
+                self.horizontal_move_flag = self.vertical_move_flag = 0
+            elif event.key == pygame.K_f:
+                self.game_and = True
 
-    def __move_objects(self):
-        self.__player.move()
+    def logek(self, width, heidth, width_rect=20, heidth_rect=10, ):
+        if self.x_rect < 0:
+            self.x_rect = 0
+        elif self.x_rect > width - width_rect:
+            self.x_rect = width - width_rect
+        if self.y_rect < 0:
+            self.y_rect = 0
+        elif self.y_rect > heidth - heidth_rect:
+            self.y_rect = heidth - heidth_rect
 
-    def __draw(self):
-        self.__screen.blit(self.__bg_sprite, (0, 0))
-        self.__player.draw(self.__screen)
-        self.__enemy.draw(self.__screen)
-        pygame.display.flip()
+    # def rect2(self, x_rect2=340, y_rect2=200, color3=(255, 0, 0), width_rect2=20, heidth_cert2=10):
+    #     pygame.draw.rect(self.screen, color3, (x_rect2, y_rect2, width_rect2, heidth_cert2))
+
+    def move2(self):
+        self.x_rect2 += self.speed2 * self.horizontal_move_flag2
+        self.y_rect2 += self.speed2 * self.vertical_move_flag2
+        self.horizontal_move_flag2 = 0
+        self.vertical_move_flag2 = 0
+
+    def event2(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_j:
+                self.horizontal_move_flag2 = -1
+            elif event.key == pygame.K_l:
+                self.horizontal_move_flag2 = 1
+            elif event.key == pygame.K_i:
+                self.vertical_move_flag2 = -1
+            elif event.key == pygame.K_k:
+                self.vertical_move_flag2 = 1
+            elif event.key == pygame.K_q:
+                self.horizontal_move_flag2 = self.vertical_move_flag2 = 0
+            elif event.key == pygame.K_f:
+                self.game_and = True
+
+    def logek2(self, width, heidth, width_rect2=20, heidth_rect2=10):
+        if self.x_rect2 < 0:
+            self.x_rect2 = 0
+        elif self.x_rect2 > width - width_rect2:
+            self.x_rect2 = width - width_rect2
+        if self.y_rect2 < 0:
+            self.y_rect2 = 0
+        elif self.y_rect2 > heidth - heidth_rect2:
+            self.y_rect2 = heidth - heidth_rect2
+
+    def collision(self, rect, rect2):
+        if rect.colliderect(rect2):
+            #self.game_and = True
+            self.count += 1
+        if self.count == 3:
+            self.game_and = True
 
 
-def main():
-    game = Game()
-    game.run()
 
-
-if __name__ == "__main__":
-    main()
+p = Game()
+p.zikl()
--- sergey.py
+++ sergey.py
@@ -4,6 +4,7 @@ import pygame
 class Game:
     def __init__(self, w=800, h=400, c=(80, 22, 100), fps=50):
         pygame.init()
+        self.count = 0
         self.width = w
         self.heidth = h
         self.color = c
@@ -15,67 +16,82 @@ class Game:
         self.y_rect = self.heidth // 2
         self.x_rect2 = 340
         self.y_rect2 = 200
+        self.width_rect = 20
+        self.heidth_cert = 10
+        self.color2 = (255, 0, 0)
+        self.rect= pygame.Rect
+        self.width_rect2 = 20
+        self.heidth_cert2 = 10
+        self.color3 = (0, 0, 255)
+        self.rect2 = pygame.Rect
         self.speed = 10
         self.speed2 = 10
         self.horizontal_move_flag = self.vertical_move_flag = 0
         self.horizontal_move_flag2 = self.vertical_move_flag2 = 0
+
+    def func_rect(self):
+        self.rect = pygame.draw.rect(self.screen, self.color2,
+                                      (self.x_rect, self.y_rect, self.width_rect2, self.heidth_cert2))
+
+    def func_rect2(self):
+        self.rect2 = pygame.draw.rect(self.screen, self.color3,
+                                      (self.x_rect2, self.y_rect2, self.width_rect2, self.heidth_cert2))
+
     def draw(self):
 
         self.screen.fill(self.color)
-        self.rect(self.x_rect, self.y_rect)
-        self.rect2()
+        self.func_rect()
+        self.func_rect2()
         pygame.display.flip()
 
     def zikl(self):
         while not self.game_and:
-            self.draw()
+
             self.clock.tick(self.fps)
             self.check_event()
-            self.check_event2()
             self.move()
             self.move2()
             self.logek2(self.width, self.heidth)
             self.logek(self.width, self.heidth)
+            self.draw()
+            self.collision(self.rect, self.rect2)
+
 
     def __del__(self):
         pygame.quit()
 
     def check_event(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event == pygame.QUIT:
                 self.game_and = True
             self.event(event)
-    def check_event2(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.game_and = True
             self.event2(event)
 
-    def rect(self, x_rect, y_rect, color2=(255, 0, 0), width_rect=20, heidth_cert=10):
-        pygame.draw.rect(self.screen, color2, (x_rect, y_rect, width_rect, heidth_cert))
+    # def rect(self, x_rect, y_rect, color2=(255, 0, 0), width_rect=20, heidth_cert=10):
+    #     self.my_rect = pygame.draw.rect(self.screen, color2, (x_rect, y_rect, width_rect, heidth_cert))
 
     def move(self):
         self.x_rect += self.speed * self.horizontal_move_flag
         self.y_rect += self.speed * self.vertical_move_flag
-        self.__horizontal_move_flag = 0
-        self.__vertical_move_flag = 0
+        self.horizontal_move_flag = 0
+        self.vertical_move_flag = 0
 
     def event(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 self.horizontal_move_flag = -1
-            elif event.key == pygame.K_d:
+            elif event.key == pygame.K_RIGHT:
                 self.horizontal_move_flag = 1
-            elif event.key == pygame.K_w:
+            elif event.key == pygame.K_UP:
                 self.vertical_move_flag = -1
-            elif event.key == pygame.K_s:
+            elif event.key == pygame.K_DOWN:
                 self.vertical_move_flag = 1
-            elif event.key == pygame.K_q:
+            elif event.key == pygame.K_SPACE:
                 self.horizontal_move_flag = self.vertical_move_flag = 0
             elif event.key == pygame.K_f:
                 self.game_and = True
 
-    def logek(self, width, heidth, width_rect=20, heidth_rect=10):
+    def logek(self, width, heidth, width_rect=20, heidth_rect=10, ):
         if self.x_rect < 0:
             self.x_rect = 0
         elif self.x_rect > width - width_rect:
@@ -85,15 +101,14 @@ class Game:
         elif self.y_rect > heidth - heidth_rect:
             self.y_rect = heidth - heidth_rect
 
-    def rect2(self, x_rect2=340, y_rect2=200, color3=(255, 0, 0), width_rect2=20, heidth_cert2=10):
-        pygame.draw.rect(self.screen, color3, (x_rect2, y_rect2, width_rect2, heidth_cert2))
+    # def rect2(self, x_rect2=340, y_rect2=200, color3=(255, 0, 0), width_rect2=20, heidth_cert2=10):
+    #     pygame.draw.rect(self.screen, color3, (x_rect2, y_rect2, width_rect2, heidth_cert2))
 
     def move2(self):
         self.x_rect2 += self.speed2 * self.horizontal_move_flag2
         self.y_rect2 += self.speed2 * self.vertical_move_flag2
         self.horizontal_move_flag2 = 0
         self.vertical_move_flag2 = 0
-
 
     def event2(self, event):
         if event.type == pygame.KEYDOWN:
@@ -109,6 +124,7 @@ class Game:
                 self.horizontal_move_flag2 = self.vertical_move_flag2 = 0
             elif event.key == pygame.K_f:
                 self.game_and = True
+
     def logek2(self, width, heidth, width_rect2=20, heidth_rect2=10):
         if self.x_rect2 < 0:
             self.x_rect2 = 0
@@ -118,5 +134,15 @@ class Game:
             self.y_rect2 = 0
         elif self.y_rect2 > heidth - heidth_rect2:
             self.y_rect2 = heidth - heidth_rect2
+
+    def collision(self, rect, rect2):
+        if rect.colliderect(rect2):
+            #self.game_and = True
+            self.count += 1
+        if self.count == 3:
+            self.game_and = True
+
+
+
 p = Game()
 p.zikl()
